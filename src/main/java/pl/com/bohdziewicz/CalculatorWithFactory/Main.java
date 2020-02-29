c28443cf1560ca2058661a6c9706af2269e23315
AsystentRoberta <asystent.roberta@gmail.com>
2020-02-29 12:50:51 +0100

    Initial commit

---
+++ src/main/java/pl/com/bohdziewicz/CalculatorWithFactory/Main.java
@@ -0,0 +1,8 @@
+package pl.com.bohdziewicz.CalculatorWithFactory;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("This is a project for educational purposes. \nI have to repeat some \"KnowHow\".");
+  }
+}
